6f8478dd88978801a1a4adf55e542cc3d15da598
valkyrie_pilot <valk@randomairborne.dev>
2021-11-16 17:32:14 -0700

    use fetch rather then xmlhttp

--- src/invite/invite.js
+++ src/invite/invite.js
@@ -3,23 +3,27 @@ function trimTrailingChars(s, charToTrim) {
     return s.replace(regExp, "");
 }
 
-function authAndAcceptInvite() {
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://api.ferris.chat/v0/auth");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("Email", document.getElementById("email").value)
-    xhr.setRequestHeader("Password", document.getElementById("password").value)
-    xhr.send("")
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let json = JSON.parse(xhr.responseText);
-            console.log(xhr.responseText)
-            let xhr = new XMLHttpRequest();
-            let code = trimTrailingChars(window.location.pathname.split("/").at(-1), "/");
-            xhr.open("POST", "https://api.ferris.chat/v0/invite/" + code, true);
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.setRequestHeader("Authorization", json['token'])
-            xhr.send("")
+async function authAndAcceptInvite() {
+    await fetch("https://api.ferris.chat/v0/auth", {
+            method: 'POST',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json',
+                'Email': document.getElementById("email").value,
+                'Password': document.getElementById("password").value
+            }
         }
-    };
+    )
+        .then(response => async function () {
+            let json = await response.json()
+
+            let code = trimTrailingChars(window.location.pathname.split("/").at(-1), "/");
+            await fetch("https://api.ferris.chat/v0/invite" + code, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': json['token']
+                }
+            })
+        })
 }
